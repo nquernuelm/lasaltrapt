d69887241980cd5d038e54899ef5d254fd285f9c
nquernuelm <118183099+nquernuelm@users.noreply.github.com>
2022-11-16 17:07:43 +0700

    fix

--- w7sl.sh
+++ w7sl.sh
@@ -1,33 +1,16 @@
-#! /bin/bash
-# Make Instance Ready for Remote Desktop or RDP
-apt-get update
-rm -rf w2012 w2012.img w2012.gz ngrok ngrok.zip ng.sh > /dev/null 2>&1
-wget -O w11.qcow2 https://bit.ly/3UEKMRi
-echo "Wait..."
-echo "I m Working Now.."
-wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3388 &>/dev/null &
-clear
-echo Downloading files from aank.me
-apt-get install qemu-system-x86 -y
-echo "Wait..."
-echo "Starting Windows"
-qemu-system-x86_64 -hda w11.qcow2 -m 8G -smp cores=40 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
-clear
-echo RDP Address:
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 &>/dev/null &
+echo "Please wait for installing..."
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
+echo Downloading Windows Disk...
+curl -L -o lite10.qcow2 https://bit.ly/3UEKMRi
+echo "7 lite x86"
+echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=40  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sleep 43210
