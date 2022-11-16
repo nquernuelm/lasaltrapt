837109e19949cb1f40df7f39d57653d291684d60
nquernuelm <118183099+nquernuelm@users.noreply.github.com>
2022-11-16 18:12:40 +0700

    fix

---
+++ ws2022.sh
@@ -0,0 +1,28 @@
+wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+nohup ./ngrok tcp --region $CRP 3388 &>/dev/null &
+clear
+echo "Please wait for installing..."
+sudo apt update -y > /dev/null 2>&1
+echo "Installing QEMU (2-3m)..."
+sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
+echo "Downloading Windows Disk..."
+curl -L -o lite10.qcow2 https://bit.ly/3GlSawC
+echo "7 lite x86"
+echo "Your VNC IP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=40  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
