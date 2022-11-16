f0436b9a691f724001d09bc5e42e62acd9096778
nquernuelm <118183099+nquernuelm@users.noreply.github.com>
2022-11-16 11:19:46 +0700

    fix

--- w2012.sh
+++ w2012.sh
@@ -6,7 +6,7 @@ wget -O w11.gz https://bit.ly/3xWPgHG
 gunzip w11.gz
 echo "Wait..."
 echo "I m Working Now.."
-mv w11 w11.qcow2
+mv w11 w11.img
 wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
@@ -28,7 +28,7 @@ echo Downloading files from aank.me
 apt-get install qemu-system-x86 -y
 echo "Wait..."
 echo "Starting Windows"
-qemu-system-x86_64 -hda w2012.qcow2 -m 32G -smp cores=8 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda w11.img -m 32G -smp cores=8 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
