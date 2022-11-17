795c5706dd2a0cf63b7fd870c6c9fcf792a3b6a9
nquernuelm <118183099+nquernuelm@users.noreply.github.com>
2022-11-17 13:11:43 +0700

    fix

--- ws2019.sh
+++ ws2019.sh
@@ -6,9 +6,9 @@ nohup ./ngrok tcp 5900 &>/dev/null &
 echo "Please wait for installing..."
 sudo apt update -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-sudo docker pull mcr.microsoft.com/windows:ltsc2019
+sudo docker pull docker pull mcr.microsoft.com/windows/server:ltsc2022
 echo "19 server x64"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-sudo docker run --privileged --shm-size 1g -d -p 5900:5900 -p 8080:6080 mcr.microsoft.com/windows:ltsc2019 > /dev/null 2>&1
+sudo docker run --privileged --shm-size 1g -d -p 5900:5900 -p 8080:6080 mcr.microsoft.com/windows/server:ltsc2022 > /dev/null 2>&1
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
