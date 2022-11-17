a5c3b98b87c7fc2b20dac7207fb3f60aea27e990
nquernuelm <118183099+nquernuelm@users.noreply.github.com>
2022-11-17 10:17:33 +0700

    fix

--- w7sl.sh
+++ w7sl.sh
@@ -10,7 +10,7 @@ sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o lite10.qcow2 https://bit.ly/3UEKMRi
 echo "7 lite x86"
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=40  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=40  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
