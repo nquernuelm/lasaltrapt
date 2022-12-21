2fe9a8137eee06bf32322bc71837ab5bc0c5eece
nquernuelm <118183099+nquernuelm@users.noreply.github.com>
2022-12-21 17:59:09 +0700

    fix

---
+++ lainux.sh
@@ -0,0 +1,40 @@
+echo -e "$123\n$123\n" | sudo passwd
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngr0k > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3388 &>/dev/null &
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull linuxserver/rdesktop
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo "Username : abc"
+echo "Password : abc"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+docker run --rm -p 3388:3389 lscr.io/linuxserver/rdesktop:ubuntu-kde > /dev/null 2>&1
+b='\033[1m'
+r='\E[31m'
+g='\E[32m'
+c='\E[36m'
+endc='\E[0m'
+enda='\033[0m'
